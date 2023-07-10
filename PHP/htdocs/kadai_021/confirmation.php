c20a6867740756ec7a29b1f14a9ce04741918af1
JIKim0303 <j_inoue9133@outlook.com>
2023-07-10 21:39:09 +0900

    kadairevised

--- PHP/htdocs/kadai_021/confirmation.php
+++ PHP/htdocs/kadai_021/confirmation.php
@@ -7,12 +7,21 @@
 </head>
 <body>
     <h2>入力内容をご確認ください。</h2>
-    <p>お名前：<?php echo $_POST['user_name'];
+    <p>お名前：<?php 
                         if(mb_strlen($_POST['user_name']) > 10) {
                             echo '文字数がオーバーしています。';
+                        } else {
+                            echo $_POST['user_name'];
                         }
                         ?></p>
     <p>性別：<?php echo $_POST['user_gender'];?></p>
-    <p>Eメールアドレス：<?php echo $_POST['user_email'];?></p>
+    <p>Eメールアドレス：<?php
+                            $emailcheck = $_POST['user_email'];
+                            if(preg_match_all('/[a-zA-Z0-9.][^!^?]+@[a-zA-Z0-9.]+/', $emailcheck)) { //[^!^?]は無効だった。
+                                echo $_POST['user_email'];
+                            } else {
+                                echo '入力できない記号が含まれています。';
+                            }
+                            ?></p>
 </body>
 </html>
